c9d11671a8602360ea143e94a21b070011e24c4d
watsonzr <watsonzr@rose-hulman.edu>
2018-05-21 19:39:19 -0400

    m3 without writing tests

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  FOR and WHILE loops.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Zack Watson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -208,7 +208,7 @@ def practice_problem3(start, n, threshold):
       :type threshold: float
     """
     ####################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ####################################################################
@@ -216,6 +216,18 @@ def practice_problem3(start, n, threshold):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ####################################################################
+
+    seq = []
+    if threshold > math.sqrt(2):
+        for i in range(n):
+            seq = seq + [start + i]
+        return seq
+    while True:
+        if len(seq) == n:
+            return seq
+        if math.sin(start) + math.cos(start) > threshold:
+            seq = seq + [start]
+        start = start + 1
 
 
 # ----------------------------------------------------------------------
